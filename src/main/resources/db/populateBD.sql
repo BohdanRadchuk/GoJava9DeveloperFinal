8399f09cf43061a26bd01d4cec857b48274ccd0d
Sasha Kaminska <sasha249133@ukr.net>
2018-05-14 17:17:55 +0300

    added default values

--- src/main/resources/db/populateBD.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO company.employees (`email`, `password`, `first_name`, `last_name`, `address`, `phone`, `salary`)
-VALUES ('first@mail.com', '123', 'First', 'Last', 'kyiv, hreshchatik 1', '0971234657', '55');
-
-INSERT INTO company.roles (name) VALUES ('ADMIN'), ('MODERATOR'), ('WORKER');
-
-INSERT INTO company.events (name) VALUES ('Working'), ('Studying');
-
-INSERT INTO company.status (name) VALUES ('Working'), ('Sick leave'), ('Vacation');
