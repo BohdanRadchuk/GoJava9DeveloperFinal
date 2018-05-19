07be6d9fd5e339fe3a5b3ce0756944aa9da5d797
Sasha Kaminska <sasha249133@ukr.net>
2018-05-19 22:11:03 +0300

    add method to SwaggerConfig

--- src/main/java/com/javanine/finalProject/config/SwaggerConfig.java
+++ src/main/java/com/javanine/finalProject/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.javanine.finalProject.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -34,5 +35,13 @@ public class SwaggerConfig extends WebMvcConfigurationSupport {
                 .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
                 .contact(new Contact("java_nine", "https://java_nine", "java_nine@gmail.com"))
                 .build();
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").
+                addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
